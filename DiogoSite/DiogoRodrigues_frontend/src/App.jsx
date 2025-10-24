b89704b908931778535ae2543c6568aab111e268
Diogo Rodrigues <98818862+FulLshoTz@users.noreply.github.com>
2025-10-24 15:35:28 +0100

    Create App.jsx

---
+++ DiogoSite/DiogoRodrigues_frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import YouTubeSection from './components/YouTubeSection.jsx'
+
+export default function App() {
+  return (
+    <div style={{ padding: '1rem', fontFamily: 'sans-serif' }}>
+      <YouTubeSection />
+    </div>
+  )
+}
