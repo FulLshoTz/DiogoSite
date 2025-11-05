b3b8a45e033000ad904654c5a4dac9740daaacd2
Diogo Rodrigues <98818862+FulLshoTz@users.noreply.github.com>
2025-11-05 15:49:50 +0000

    Update main.jsx

--- DiogoSite/DiogoRodrigues_frontend/src/main.jsx
+++ DiogoSite/DiogoRodrigues_frontend/src/main.jsx
@@ -1,13 +1,78 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { AnimatePresence, motion } from "framer-motion";
 import App from "./App";
+import Corridas from "./pages/Corridas";
+import Circuitos from "./pages/Circuitos";
+import Definicoes from "./pages/Definicoes";
 import "./index.css";
 
+function AnimatedRoutes() {
+  const location = useLocation();
+
+  return (
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route
+          path="/"
+          element={
+            <motion.div
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -20 }}
+              transition={{ duration: 0.4 }}
+            >
+              <App />
+            </motion.div>
+          }
+        />
+        <Route
+          path="/corridas"
+          element={
+            <motion.div
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -20 }}
+              transition={{ duration: 0.4 }}
+            >
+              <Corridas />
+            </motion.div>
+          }
+        />
+        <Route
+          path="/circuitos"
+          element={
+            <motion.div
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -20 }}
+              transition={{ duration: 0.4 }}
+            >
+              <Circuitos />
+            </motion.div>
+          }
+        />
+        <Route
+          path="/definicoes"
+          element={
+            <motion.div
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -20 }}
+              transition={{ duration: 0.4 }}
+            >
+              <Definicoes />
+            </motion.div>
+          }
+        />
+      </Routes>
+    </AnimatePresence>
+  );
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <AnimatedRoutes />
+  </BrowserRouter>
 );
