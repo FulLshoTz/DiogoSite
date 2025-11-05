095275476a86ef1e415332da2a66ec0b10c2084a
Diogo Rodrigues <98818862+FulLshoTz@users.noreply.github.com>
2025-11-05 12:57:12 +0000

    Update App.jsx

--- DiogoSite/DiogoRodrigues_frontend/src/App.jsx
+++ DiogoSite/DiogoRodrigues_frontend/src/App.jsx
@@ -1,28 +1,23 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import Corridas from "./pages/Corridas";
 import Circuitos from "./pages/Circuitos";
 import Definicoes from "./pages/Definicoes";
-import "./index.css";
 
-function App() {
+export default function App() {
   return (
-    <Router>
-      <div className="min-h-screen bg-carbon text-white font-fytyra">
-        <Header />
-        <main className="pt-20 px-4 sm:px-8">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/corridas" element={<Corridas />} />
-            <Route path="/circuitos" element={<Circuitos />} />
-            <Route path="/definicoes" element={<Definicoes />} />
-          </Routes>
-        </main>
-      </div>
-    </Router>
+    <div className="min-h-screen flex flex-col bg-neutral-950 text-white">
+      <Header />
+      <main className="flex-grow px-6 md:px-12 lg:px-20 py-8">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/corridas" element={<Corridas />} />
+          <Route path="/circuitos" element={<Circuitos />} />
+          <Route path="/definicoes" element={<Definicoes />} />
+        </Routes>
+      </main>
+    </div>
   );
 }
-
-export default App;
