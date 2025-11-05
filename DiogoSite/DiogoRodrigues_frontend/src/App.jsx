6adec496ffcfe7505807f44536a981bfb3725fcb
Diogo Rodrigues <98818862+FulLshoTz@users.noreply.github.com>
2025-11-05 12:40:23 +0000

    Update App.jsx

--- DiogoSite/DiogoRodrigues_frontend/src/App.jsx
+++ DiogoSite/DiogoRodrigues_frontend/src/App.jsx
@@ -1,17 +1,28 @@
-import React, { useEffect, useState } from "react"
-import Header from "./components/Header"
-import Navbar from "./components/Navbar"
-import YouTubeSection from "./components/YouTubeSection"
-import "./index.css"
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Corridas from "./pages/Corridas";
+import Circuitos from "./pages/Circuitos";
+import Definicoes from "./pages/Definicoes";
+import "./index.css";
 
-export default function App() {
+function App() {
   return (
-    <div className="min-h-screen bg-carbon text-white">
-      <Header />
-      <Navbar />
-      <main className="p-4 md:p-8">
-        <YouTubeSection />
-      </main>
-    </div>
-  )
+    <Router>
+      <div className="min-h-screen bg-carbon text-white font-fytyra">
+        <Header />
+        <main className="pt-20 px-4 sm:px-8">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/corridas" element={<Corridas />} />
+            <Route path="/circuitos" element={<Circuitos />} />
+            <Route path="/definicoes" element={<Definicoes />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
+  );
 }
+
+export default App;
