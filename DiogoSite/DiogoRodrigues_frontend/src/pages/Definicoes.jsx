7b15657b6fffd571e626e876cebf0237e94df0bd
Diogo Rodrigues <98818862+FulLshoTz@users.noreply.github.com>
2025-11-05 12:53:54 +0000

    Create Definicoes.jsx

---
+++ DiogoSite/DiogoRodrigues_frontend/src/pages/Definicoes.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Definicoes() {
+  return (
+    <div className="text-center mt-20">
+      <h1 className="text-3xl font-semibold text-red-500 mb-4">Definições</h1>
+      <p className="text-gray-400">Configurações personalizadas e opções do simulador.</p>
+    </div>
+  );
+}
