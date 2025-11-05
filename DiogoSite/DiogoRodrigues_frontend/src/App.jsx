d0631f5a7be1ecf06d855dfe188fd1a1bbac0483
Diogo Rodrigues <98818862+FulLshoTz@users.noreply.github.com>
2025-11-05 16:14:19 +0000

    Update App.jsx

--- DiogoSite/DiogoRodrigues_frontend/src/App.jsx
+++ DiogoSite/DiogoRodrigues_frontend/src/App.jsx
@@ -1,12 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import YoutubeSection from "./components/YoutubeSection";
+import Corridas from "./pages/Corridas";
+import Circuitos from "./pages/Circuitos";
+import Definicoes from "./pages/Definicoes";
 
 export default function App() {
   return (
-    <main className="pt-24">
+    <Router>
       <Header />
-      <YoutubeSection />
-    </main>
+      <main className="pt-24 px-4">
+        <Routes>
+          <Route path="/" element={<YoutubeSection />} />
+          <Route path="/corridas" element={<Corridas />} />
+          <Route path="/circuitos" element={<Circuitos />} />
+          <Route path="/definicoes" element={<Definicoes />} />
+        </Routes>
+      </main>
+    </Router>
   );
 }
