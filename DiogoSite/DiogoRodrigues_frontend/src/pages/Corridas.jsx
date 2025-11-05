c08c39430f5c487116d6fffe3122c36c2f6e132d
Diogo Rodrigues <98818862+FulLshoTz@users.noreply.github.com>
2025-11-05 12:53:20 +0000

    Create Corridas.jsx

---
+++ DiogoSite/DiogoRodrigues_frontend/src/pages/Corridas.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Corridas() {
+  return (
+    <div className="text-center mt-20">
+      <h1 className="text-3xl font-semibold text-red-500 mb-4">Corridas</h1>
+      <p className="text-gray-400">Aqui vais poder ver estatísticas e resultados das tuas corridas.</p>
+    </div>
+  );
+}
