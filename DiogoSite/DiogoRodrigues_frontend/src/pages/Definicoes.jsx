e8cfd49a8fbdca1c281d72eedda7dcd68992c567
Diogo Rodrigues <98818862+FulLshoTz@users.noreply.github.com>
2025-11-05 15:20:00 +0000

    Update Definicoes.jsx

--- DiogoSite/DiogoRodrigues_frontend/src/pages/Definicoes.jsx
+++ DiogoSite/DiogoRodrigues_frontend/src/pages/Definicoes.jsx
@@ -2,9 +2,12 @@ import React from "react";
 
 export default function Definicoes() {
   return (
-    <div className="text-center mt-20">
-      <h1 className="text-3xl font-semibold text-red-500 mb-4">Definições</h1>
-      <p className="text-gray-400">Configurações personalizadas e opções do simulador.</p>
-    </div>
+    <section className="pt-28 max-w-7xl mx-auto text-center">
+      <h2 className="text-4xl font-bold text-red-600 mb-6">Definições</h2>
+      <p className="text-gray-400">
+        Aqui poderás ajustar preferências, layouts e definições pessoais.
+        (Em breve)
+      </p>
+    </section>
   );
 }
