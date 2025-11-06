4b805a830d2551ca4c0e5d5fac5479a1fcb8c413
Diogo Rodrigues <98818862+FulLshoTz@users.noreply.github.com>
2025-11-06 16:03:15 +0000

    Update tailwind.config.js

--- DiogoSite/DiogoRodrigues_frontend/tailwind.config.js
+++ DiogoSite/DiogoRodrigues_frontend/tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: false, // 🚫 desativa modo escuro automático
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
