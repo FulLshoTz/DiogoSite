bdc1961d98ed6362debf3e99c8653daf577ab210
Diogo Rodrigues <98818862+FulLshoTz@users.noreply.github.com>
2025-11-07 12:42:31 +0000

    Update YoutubeSection.jsx

--- DiogoSite/DiogoRodrigues_frontend/src/sections/YoutubeSection.jsx
+++ DiogoSite/DiogoRodrigues_frontend/src/sections/YoutubeSection.jsx
@@ -56,8 +56,7 @@ export default function YoutubeSection() {
   return (
     <>
       <ChannelHeader />
-
-      <section className="max-w-7xl mx-auto text-white px-4 py-8">
+      <section className="max-w-7xl mx-auto text-white px-4 py-6 mt-0">
         {/* Título com ícone + linha */}
         <div className="flex items-center gap-3 mb-6">
           <svg className="w-6 h-6 text-red-600" viewBox="0 0 576 512" fill="currentColor">
