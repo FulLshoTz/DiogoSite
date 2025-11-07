c06f3507fd08767ec9f9bc129ff5aa97bb6fc171
Diogo Rodrigues <98818862+FulLshoTz@users.noreply.github.com>
2025-11-07 13:06:36 +0000

    Update App.jsx

--- DiogoSite/DiogoRodrigues_frontend/src/App.jsx
+++ DiogoSite/DiogoRodrigues_frontend/src/App.jsx
@@ -33,7 +33,7 @@ function App() {
           <Sidebar />
           <div className="flex-1 flex flex-col">
             <Header />
-            <main className="flex-1 pt-4">
+            <main className="flex-1 pt-18">
               <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/corridas" element={<Corridas />} />
