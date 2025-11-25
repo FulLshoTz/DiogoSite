8fb31d0969e60cc3c2e4c61fe5ac8df6936297e5
Diogo Rodrigues <98818862+FulLshoTz@users.noreply.github.com>
2025-11-25 17:26:11 +0000

    Update backend.js

--- DiogoSite/DiogoRodrigues_frontend/src/services/backend.js
+++ DiogoSite/DiogoRodrigues_frontend/src/services/backend.js
@@ -1,9 +1,10 @@
 const BACKEND_URL = "https://diogorodrigues-backend.onrender.com";
 
 export async function getLatestVideos() {
-  const res = await fetch(`${BACKEND_URL}/api/latest-videos`);
+  const res = await fetch("https://diogorodrigues-backend.onrender.com/api/latest-videos");
   return res.json();
 }
+
 
 // REMOVE ISTO (o backend não tem esta rota!)
 // export async function getLiveStatus() {
