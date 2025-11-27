96c351074903fb9044264d54e6546227718dc6fb
Diogo Rodrigues <98818862+FulLshoTz@users.noreply.github.com>
2025-11-27 13:36:50 +0000

    Update Home.jsx

--- DiogoSite/DiogoRodrigues_frontend/src/pages/Home.jsx
+++ DiogoSite/DiogoRodrigues_frontend/src/pages/Home.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import YoutubeSection from "../sections/YoutubeSection";
-import StatsSection from "../sections/StatsSection";
-import RacesSection from "../sections/RacesSection";
+import NoticiasSection from "../sections/NoticiasSection";
 
 export default function Home() {
   return (
     <div className="space-y-16">
       <YoutubeSection />
-     <StatsSection />
-   <RacesSection />
+      <NoticiasSection />
     </div>
   );
 }
