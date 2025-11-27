ff7c5293e1016fa10286871cb39e7159fa5705ca
Diogo Rodrigues <98818862+FulLshoTz@users.noreply.github.com>
2025-11-27 16:01:39 +0000

    Update Home.jsx

--- DiogoSite/DiogoRodrigues_frontend/src/pages/Home.jsx
+++ DiogoSite/DiogoRodrigues_frontend/src/pages/Home.jsx
@@ -4,8 +4,13 @@ import NoticiasSection from "../sections/NoticiasSection";
 
 export default function Home() {
   return (
-    <div className="space-y-16">
+    // 4. Reduzi de space-y-16 para space-y-8 para diminuir o buraco
+    <div className="space-y-8 pb-10">
       <YoutubeSection />
+      
+      {/* 11. Se tivesses aqui <RacesSection /> ou <StatsSection />, bastava comentar assim: */}
+      {/* <StatsSection /> */}
+      
       <NoticiasSection />
     </div>
   );
