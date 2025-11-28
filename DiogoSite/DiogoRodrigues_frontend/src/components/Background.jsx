92c21618e1f1478a60411c7420b9dc638d9324c3
Diogo Rodrigues <98818862+FulLshoTz@users.noreply.github.com>
2025-11-28 10:21:37 +0000

    Update Background.jsx

--- DiogoSite/DiogoRodrigues_frontend/src/components/Background.jsx
+++ DiogoSite/DiogoRodrigues_frontend/src/components/Background.jsx
@@ -1,29 +1,26 @@
 import React from "react";
+// IMPORTANTE: Importar a imagem da pasta assets
+// O "../" significa "voltar atrás uma pasta" (sair de components para ir a assets)
+import carbonTexture from "../assets/carbon.jpg"; 
 
 export default function Background() {
   return (
-    <div className="fixed top-0 left-0 w-full h-full -z-50 bg-black">
+    <div className="fixed top-0 left-0 w-full h-full -z-50 bg-[#111]">
       
       {/* 1. A IMAGEM DE CARBONO */}
       <div
         className="absolute inset-0 w-full h-full"
         style={{
-          // O caminho começa com / porque está na pasta public
-          backgroundImage: "url('/carbon.jpg')", 
+          // Agora usamos a variável importada em vez de texto fixo
+          backgroundImage: `url(${carbonTexture})`, 
           
-          // 'repeat' faz com que a imagem se repita como azulejos (melhor qualidade)
           backgroundRepeat: "repeat", 
-          
-          // Tamanho do padrão. Ajusta isto se o carbono parecer muito grande ou pequeno
-          backgroundSize: "200px", 
-          
-          // Opacidade para não ficar demasiado berrante e misturar com o preto
+          backgroundSize: "200px", // Ajusta o tamanho do padrão aqui
           opacity: 0.6 
         }}
       />
 
       {/* 2. SOMBRA NOS CANTOS (Vignette) */}
-      {/* Isto é crucial para o site parecer profissional e focar o centro */}
       <div 
         className="absolute inset-0 w-full h-full"
         style={{
@@ -31,8 +28,7 @@ export default function Background() {
         }}
       />
       
-      {/* 3. MÁSCARA ESCURA (Opcional) */}
-      {/* Se a imagem for muito clara, isto escurece tudo uniformemente */}
+      {/* 3. MÁSCARA ESCURA */}
       <div className="absolute inset-0 bg-black/40" />
 
     </div>
